7a6d35aa5dbc0fa4121e28c0737133b7c0dac453
Salva <30794365+OcZi@users.noreply.github.com>
2023-07-19 01:30:38 -0500

    Add files via upload

---
+++ demo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int fibonacci(int i) {
+    if (i < 1) {
+        return 0;
+    } else if (i == 1) {
+        return 1;
+    }
+
+    return fibonacci(i - 1) + fibonacci(i - 2);
+}
+
+int main() {
+  std::string name;
+  std::cout << "Name: ";
+  std::cin >> name;
+  std::cout << "\n";
+  std::cout << "Hello " << name << "\n";
+  std::cout << "Fibonacci from 1 to 20:" << "\n";
+  for (int i = 1; i <= 20; i++) {
+    std::cout << fibonacci(i) << "\n";
+  }
+}
